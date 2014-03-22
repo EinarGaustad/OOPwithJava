64d9e8f06735a7d93758bedef0a4f0995b2ac463
Guo Junjun <guojunjun@gmail.com>
2014-03-23 00:25:00 +0100

    Credit Account

--- oving9/abstractAccount/CreditAccount.java
+++ oving9/abstractAccount/CreditAccount.java
@@ -16,5 +16,48 @@ package oving9.abstractAccount;
  *         Konstruktøren CreditAccount(double) skal sette kredittlinjen.
  */
 public class CreditAccount extends AbstractAccount {
-	
+    
+    private double credit;
+    
+    CreditAccount(double credit) {
+        if (credit < 0) {
+            throw new IllegalArgumentException(
+                    "credit must always greater or equal to 0");
+        } else {
+            this.credit = credit;
+        }
+    }
+    
+    void setCreditLine(double credit) {
+        if (credit < 0) {
+            throw new IllegalArgumentException("Can not set negativ credit");
+        } else if (credit + belop < 0) {
+            throw new IllegalStateException(
+                    "New credit must at least cover negativ amount");
+        } else {
+            
+            this.credit = credit;
+        }
+    }
+    
+    double getCreditLine() {
+        return this.credit;
+    }
+
+    @Override
+    void internalWithdraw(double belop) {
+        if (belop > this.belop + this.credit) {
+            throw new IllegalStateException("Not enought amount to withdraw");
+        } else if (belop <= 0) {
+            throw new IllegalArgumentException(
+                    "Can only withdraw positive amount");
+        } else {
+            this.belop -= belop;
+            // if (this.belop < 0) {
+            // this.credit -= belop;
+            // this.belop = 0;
+            // }
+        }
+    }
+
 }
