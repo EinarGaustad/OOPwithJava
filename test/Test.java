23c2850af5532b3a0b584570b0e59ede687833d9
Guo Junjun <guojunjun@gmail.com>
2014-04-23 01:13:28 +0200

    iterator

--- test/Test.java
+++ test/Test.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Test {
     
@@ -29,5 +32,35 @@ public class Test {
         char t2 = 'b';
         System.out.println((t1 + 1) == t2);
         System.out.println((t1 + 1));
+        
+        List<String> list = new ArrayList<String>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list.size());
+
+        Iterator<String> it = list.iterator(); // !
+
+        for (int j = 0; j < "abc".length(); j++) {
+            if (it.hasNext()) {
+                String s = list.iterator().next();
+                String ss = list.iterator().next();
+                System.out.println(s + " : " + it.next());
+                System.out.println(list.get(j));
+                System.out.println("2: "
+                        + ((Iterator<String>) list.iterator()).next());
+            }
+        }
+        // for (Iterator<String> it = list.iterator(); it.hasNext();) {
+        // System.out.println(it.next());
+        // }
+        for (String s : list) {
+            System.out.println("method 3: " + s);
+        }
+    }
+    
+    private static Iterator<String> iterator(List<String> list) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
